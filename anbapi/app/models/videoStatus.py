1e55ef89debcc916aea5f3a7cb19c2cfe3be1d6a
vcamachoandes <v.camacho@uniandes.edu.co>
2025-10-19 15:37:45 -0500

    delete videos funcionando :D

--- anbapi/app/models/videoStatus.py
+++ anbapi/app/models/videoStatus.py
@@ -1,7 +1,9 @@
-import enum
+from enum import Enum
 
 
-class VideoStatus(str, enum.Enum):
+class VideoStatus(str, Enum):
     UPLOADED = "uploaded"
     PROCESSED = "processed"
+    PUBLISHED = "published"
+    VOTING = "voting"
     FAILED = "failed"
