477949839eace0e05327cae1cdf902b24bb33e5c
Carlos Niño <cf.ninor1@uniandes.edu.co>
2025-11-08 05:56:16 -0500

    fix: rutas archivo

--- anbapi/app/storage_a/factory.py
+++ anbapi/app/storage_a/factory.py
@@ -8,5 +8,4 @@ def get_storage():
     backend = os.getenv("STORAGE_BACKEND", "local").lower()
     if backend == "s3":
         return S3Storage()
-    base_dir = os.getenv("UPLOAD_DIR")
-    return LocalStorage(base_dir=base_dir)
+    return LocalStorage(base_dir="/data")
