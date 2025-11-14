1de03192b51630997c116b9f75e7ab293ce68333
Carlos Niño <cf.ninor1@uniandes.edu.co>
2025-11-14 03:50:49 -0500

    fix: se retira pgbouncer

--- anbapi/app/database.py
+++ anbapi/app/database.py
@@ -6,13 +6,17 @@ from sqlalchemy.pool import QueuePool
 engine = create_engine(
     config.DATABASE_URL,
     poolclass=QueuePool,
-    pool_size=10,
-    max_overflow=10,
+    pool_size=5,
+    max_overflow=5,
+    pool_timeout=30,
     pool_pre_ping=True,
     pool_recycle=1800,
     pool_use_lifo=True,
     future=True,
-    connect_args={"connect_timeout": 5, "application_name": "api-anb"},
+    connect_args={
+        "connect_timeout": 5,
+        "application_name": "api-anb",
+    },
 )
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, future=True)
